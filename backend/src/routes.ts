0f4fb839c24c821396daea81c05a0d8ecf72f302
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-05-08 17:04:49 -0300

    update

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,7 +18,6 @@ import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 
-
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp")); //pode ser passado como um middleware
@@ -56,11 +55,20 @@ router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new DeleteOrderController().handle);
 
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
-router.post("/order/remove", isAuthenticated, new RemoveItemController().handle);
+router.post(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 
 // ROTA DE ORDER
 
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 export { router };
